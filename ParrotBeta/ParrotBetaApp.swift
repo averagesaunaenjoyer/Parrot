7d5663488c13955560e8f83ad747c29ab4c3186b
Serafin dela Paz <serafyne9@gmail.com>
2023-06-29 22:04:07 -0700

    Initial Commit

---
+++ ParrotBeta/ParrotBetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParrotBetaApp.swift
+//  ParrotBeta
+//
+//  Created by Serafin dela Paz on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ParrotBetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
